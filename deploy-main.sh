563260b41c2f98cb709e3f0b763afc5ed48f89f6
Apursuit <apursuit27@gmail.com>
2024-12-06 08:04:51 +0800

    commit

--- deploy-main.sh
+++ deploy-main.sh
@@ -37,5 +37,6 @@ cd -
 rm -rf "$dist_path"
 
 # 查看流程结果
-echo "Press [Enter] to exit (or wait 5 seconds)..."
-read -r -t 5 || echo "Timed out, exiting..."
+# echo "Press [Enter] to exit (or wait 5 seconds)..."
+# read -r -t 5 || echo "Timed out, exiting..."
+pause
